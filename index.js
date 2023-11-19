5067fe555ad933f0505a78656aea1f3ce6f6c9a1
Suraj Yadav <surajyadavy676@gmail.com>
2023-11-19 16:25:05 +0530

    this is my 2nd project

---
+++ index.js
@@ -0,0 +1,18 @@
+// const psrcContent = ['Products', 'Solutions', 'Resources', 'Company'];
+// let psrc = document.getElementById('psrc');
+// for (let i = 0; i < 4; i++) {
+//     const h3 = document.createElement('h3');
+//     h3.innerText = psrcContent[i];
+//     psrc.appendChild(h3);
+//     let products = ["Visitors",'Workplace', 'Desks', 'Rooms', 'Deliveries', 'Connect','Platform','Mobile','App marketplace']
+//     if(i == 0){
+//        for(let i = 0; i < products.length; i++){
+//         let p = document.createElement('p')
+//         p.innerText = products[i]
+//         h3.appendChild(p)
+//        } 
+//     }
+// }
+
+
+
---
+++ nevigation/nevigation.js
@@ -0,0 +1,12 @@
+let login = document.getElementById('login')
+        login.addEventListener('click',function(){
+            window.location.href = "signIn.html"
+        })
+let getStart = document.querySelector('#item>h3>button')
+        getStart.addEventListener('click',function(){
+        window.location.href = "signUp.html"
+})
+let startTrial = document.getElementById('trial')
+startTrial.addEventListener('click',function(){
+    window.location.href = "signUp.html"
+})
